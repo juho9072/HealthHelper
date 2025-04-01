c1b4f05377aabae3dc291665e6d910a3b34493a6
mjkim <alswns328@gmail.com>
2025-04-01 17:24:40 +0900

    이상한데

--- app/src/main/java/com/example/healthhelper/MainActivity.kt
+++ app/src/main/java/com/example/healthhelper/MainActivity.kt
@@ -12,7 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.healthhelper.ui.theme.HealthHelperTheme
-//aaaaaaaaaaa
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+//aaaaaaaaaaaaaaaaa
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
