2016f526bcde53b081b93ef875d678b3eadcb8dc
mjkim <alswns328@gmail.com>
2025-04-01 17:23:07 +0900

    aaaaa

--- app/src/main/java/com/example/healthhelper/MainActivity.kt
+++ app/src/main/java/com/example/healthhelper/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.healthhelper.ui.theme.HealthHelperTheme
-
+//aaaaaaaaaaa
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
