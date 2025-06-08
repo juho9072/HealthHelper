a66b322470be9297ff1c15d4f5eb49c0928866e3
kyoomyi <kyoom29@gmail.com>
2025-06-08 16:59:11 +0900

    record_page_complete

--- app/src/main/java/com/example/myapplication/RoutineItem.java
+++ app/src/main/java/com/example/myapplication/RoutineItem.java
@@ -1,13 +1,13 @@
-package com.example.myapplication;
+    package com.example.myapplication;
 
-public class RoutineItem {
-    public String title;
-    public String thumbnailUrl;
-    public String youtubeLink; // 🔹 유튜브 원본 링크 추가
+    public class RoutineItem {
+        public String title;
+        public String thumbnailUrl;
+        public String youtubeLink; // 🔹 유튜브 원본 링크 추가
 
-    public RoutineItem(String title, String thumbnailUrl, String youtubeLink) {
-        this.title = title;
-        this.thumbnailUrl = thumbnailUrl;
-        this.youtubeLink = youtubeLink;
+        public RoutineItem(String title, String thumbnailUrl, String youtubeLink) {
+            this.title = title;
+            this.thumbnailUrl = thumbnailUrl;
+            this.youtubeLink = youtubeLink;
+        }
     }
-}
